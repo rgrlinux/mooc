1d9ca5c882aeb57cbc54096c2a4b7791914b98bf
rogerio oliveira <rgrlinux@gmail.com>
2018-09-09 17:11:00 -0300

    Final

--- forum/models.py
+++ forum/models.py
@@ -9,12 +9,11 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
-        # ordering = ['-updated_at']
 
 
 class Thread(BaseModel):
-
     title = models.CharField('Título', max_length=100)
+    slug = models.SlugField('Slug', max_length=120, unique=True)
     body = models.TextField('Mensagem')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Autor', related_name='threads',
                                on_delete=models.CASCADE)
@@ -25,13 +24,17 @@ class Thread(BaseModel):
     def __str__(self):
         return self.title
 
+    @models.permalink
+    def get_absolute_url(self):
+        return 'forum:thread', (), {'slug': self.slug}
+
     class Meta:
         verbose_name = 'Tópico'
+        ordering = ['-updated_at']
 
 
 class Reply(BaseModel):
-
-    thread = models.ForeignKey(Thread, verbose_name='Tópico', related_name='threads', on_delete=models.CASCADE)
+    thread = models.ForeignKey(Thread, verbose_name='Tópico', related_name='replies', on_delete=models.CASCADE)
     reply = models.TextField('Resposta')
     author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='Autor', related_name='replies',
                                on_delete=models.CASCADE)
@@ -43,3 +46,19 @@ class Reply(BaseModel):
     class Meta:
         verbose_name = 'Resposta'
         ordering = ['-correct', 'created_at']
+
+
+def post_save_reply(instance, **kwargs):
+    instance.thread.answers = instance.thread.replies.count()
+    instance.thread.save()
+    if instance.correct:
+        instance.thread.replies.exclude(pk=instance.pk).update(correct=False)
+
+
+def post_delete_reply(instance, **kwargs) :
+    instance.thread.answers = instance.thread.replies.count()
+    instance.thread.save()
+
+
+models.signals.post_save.connect(post_save_reply, sender=Reply, dispatch_uid='post_save_replay')
+models.signals.post_delete.connect(post_delete_reply, sender=Reply, dispatch_uid='post_delete_replay')
--- forum/urls.py
+++ forum/urls.py
@@ -5,4 +5,9 @@ app_name = 'forum'
 
 urlpatterns = [
     path('', v.index, name='index'),
-    ]
+    path('tag/<str:tag>', v.index, name='index_tagged'),
+    path('respostas/<int:pk>/naocorreta/', v.ReplyCorrectView.as_view(), name='reply_incorrect'),
+    path('respostas/<int:pk>/correta/', v.ReplyCorrectView.as_view(correct=True), name='reply_correct'),
+
+    path('topico/<str:slug>', v.thread, name='thread'),
+]
